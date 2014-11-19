f9267f28a2483a3a2521f00a11aca84d0badd1c5
Mark Marijnissen <markmarijnissen@gmail.com>
2014-11-19 12:38:04 +0100

    added complete build and update readme

---
+++ bundle.js
@@ -0,0 +1,4 @@
+window.CordovaAppLoader = require('./index');
+window.CordovaFileCache = require('cordova-file-cache');
+window.CordovaPromiseFS = require('cordova-promise-fs');
+window.Promise = require('promiscuous');
